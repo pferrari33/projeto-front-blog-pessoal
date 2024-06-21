77d6c2a31cbdb39ab79c5ade164ffb65717cbc91
unknown <palomaferrari33@gmail.com>
2024-06-21 18:20:12 -0300

    primeiro componente

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+        <h1 className="home">Home</h1>
+        <img src="./public/gatinho.png" alt="gatinho-joia" className="img"/>
+        
+        </>
+    );
+}
+
+export default Home;
